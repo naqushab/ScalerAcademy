5e9fa68bf3c1861015a3cec237f79334b09992e4
Naqushab Neyazee <neyazee@adobe.com>
2021-12-23 01:39:06 +0530

    Create HW3.py

---
+++ Scaler/Maths - Modular Arithmetic Introduction/HW3.py
@@ -0,0 +1,14 @@
+class Solution:
+    # @param A : integer
+    # @param B : integer
+    # @param C : integer
+    # @param D : integer
+    # @param E : integer
+    # @param F : integer
+    # @param G : integer
+    # @param H : integer
+    # @return an integer
+    def solve(self, A, B, C, D, E, F, G, H):
+        if C <= E or G <= A or H <= B or D <= F:
+            return 0
+        return 1
