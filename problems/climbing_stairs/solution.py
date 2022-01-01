57446162157d259a214f74cdd87486c170412cd8
Naqushab Neyazee <neyazee@adobe.com>
2022-01-01 12:27:45 +0000

    Sync LeetCode submission - Climbing Stairs (python3)

--- problems/climbing_stairs/solution.py
+++ problems/climbing_stairs/solution.py
@@ -1,7 +1,16 @@
-from functools import cache
 class Solution:
-    @cache
     def climbStairs(self, n: int) -> int:
         if n <= 2:
             return n
-        return self.climbStairs(n-1) + self.climbStairs(n-2)
+        
+        dp = [-1]*(n+1)
+        dp[0] = 0
+        dp[1] = 1
+        dp[2] = 2
+        
+        def cs(n):
+            for i in range(3, n+1):
+                dp[i] = dp[i-1] + dp[i-2]
+            return dp[n]
+        
+        return dp[n] if n <= 2 else cs(n)
