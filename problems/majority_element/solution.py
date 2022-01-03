275985a6f64806ebb9861e8669f66fd9bda9e428
Naqushab Neyazee <neyazee@adobe.com>
2022-01-03 14:10:21 +0000

    Sync LeetCode submission - Majority Element (python3)

--- problems/majority_element/solution.py
+++ problems/majority_element/solution.py
@@ -1,13 +1,13 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         c = 1
-        ans = nums[0]
+        m = nums[0]
         for n in nums[1:]:
-            if n == ans:
-                c += 1
+            if n == m:
+                c+=1
             else:
-                c -= 1
-            if c == 0:
-                ans = n
+                c-=1
+            if c==0:
+                m = n
                 c = 1
-        return ans
+        return m
