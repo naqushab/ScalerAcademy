6603e11353f8c01a5c62b889630b671af265fdb5
Naqushab Neyazee <neyazee@adobe.com>
2022-01-07 21:46:21 +0000

    Sync LeetCode submission - 01 Matrix (python3)

--- problems/01_matrix/solution.py
+++ problems/01_matrix/solution.py
@@ -1,26 +1,26 @@
-from collections import deque
-
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         r = len(mat)
         c = len(mat[0])
-        q = deque()
-        ans = [[float('+inf')]*c for _ in range(r)]
+        ans = [[math.inf]*c for _ in range(r)]
+        q = collections.deque()
         
         def bfs(q):
             while q:
-                ti, tj = q.popleft()
-                for d in ((-1, 0), (1, 0), (0, -1), (0, 1)):
-                    ni, nj = ti+d[0], tj+d[1]
-                    if 0<=ni<r and 0<=nj<c:
-                        if ans[ni][nj] > ans[ti][tj] + 1:
-                            ans[ni][nj] = ans[ti][tj] + 1
-                            q.append((ni, nj))
+                ql = len(q)
+                for _ in range(ql):
+                    x, y = q.popleft()
+                    for p in ((x-1, y), (x+1, y), (x, y-1), (x, y+1)):
+                        m, n = p[0], p[1]
+                        if 0<=m<r and 0<=n<c and ans[m][n] == math.inf:
+                            ans[m][n] = ans[x][y]+1
+                            q.append((m, n))
         
-        for i in range(r):
-            for j in range(c):
-                if mat[i][j] == 0:
-                    ans[i][j] = 0
-                    q.append((i, j))
+        for i, j in product(range(r), range(c)):
+            if mat[i][j] == 0:
+                ans[i][j] = 0
+                q.append((i, j))
+        
         bfs(q)
+        
         return ans
