7ded7415f98d01aae088a7bf38b16a9364792b23
Naqushab Neyazee <neyazee@adobe.com>
2022-01-12 10:37:56 +0000

    Sync LeetCode submission - Counting Bits (python3)

--- problems/counting_bits/solution.py
+++ problems/counting_bits/solution.py
@@ -1,10 +1,9 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        ans = [0]
+        ans = [0]*(n+1)
+        offset = 1
         for i in range(1, n+1):
-            c1 = 0
-            while i > 0:
-                c1 += 1
-                i = i&i-1
-            ans.append(c1)
+            if offset*2 == i:
+                offset = i
+            ans[i] = 1+ans[i-offset]
         return ans
