38ac5e2c749e6d39cb82dd09a7978280f8ea51f2
Naqushab Neyazee <neyazee@adobe.com>
2022-01-15 09:52:20 +0000

    Sync LeetCode submission - Longest Common Subsequence (python3)

--- problems/longest_common_subsequence/solution.py
+++ problems/longest_common_subsequence/solution.py
@@ -1,11 +1,13 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        @functools.cache
-        def lcs(s1, s2, m, n):
-            if m == 0 or n == 0:
+        
+        @functools.lru_cache(None)
+        def lcs(s1, s2, i1, i2):
+            if i1 < 0 or i2 < 0:
                 return 0
-            if s1[m-1] == s2[n-1]:
-                return 1 + lcs(s1, s2, m-1, n-1)
+            if s1[i1] == s2[i2]:
+                return 1 + lcs(s1, s2, i1-1, i2-1)
             else:
-                return max(lcs(s1, s2, m, n-1), lcs(s1, s2, m-1, n))
-        return lcs(text1, text2, len(text1), len(text2))
+                return max(lcs(s1, s2, i1-1, i2), lcs(s1, s2, i1, i2-1))
+        
+        return lcs(text1, text2, len(text1)-1, len(text2)-1)
