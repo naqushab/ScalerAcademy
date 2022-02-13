5d151e1e286974cb231c57fdf2fee02e46a5bcf8
Naqushab Neyazee <neyazee@adobe.com>
2022-02-13 16:52:48 +0530

    lc

--- Leetcode/Sample1.py
+++ Leetcode/Sample1.py
@@ -1,40 +1,16 @@
-from collections import deque
+import heapq
 
-def decode_string(s):
-    ans = ''
-    st = deque()
-    num = 0
-    for ch in s:
-        if ch.isdigit():
-            num = num*10 + int(ch)
-        elif ch == '[':
-            if num > 0:
-                st.append(str(num))
-                num = 0
-            else:
-                st.append(str(1))
-        elif ch.isalpha():
-            st.append(ch)
-        elif ch == ']':
-            temp = ''
-            while len(st) > 0:
-                if st[-1].isalpha():
-                    temp += st.pop()
-                elif st[-1].isdigit():
-                    st.append(temp*int(st.pop()))
-                    break
-    ans = ''
-    while st:
-        ans += st.pop()
-    return ans[::-1]
+def solve(A):
+    running_sum = 0
+    ans = 0
+    for n in A:
+        running_sum += n
+        if running_sum <= 0:
+            ans += 1
+            running_sum -= n
+    return ans
 
-def main():
-    assert decode_string(s='3[ab2[c]]') == 'abccabccabcc'
-    assert decode_string(s='3[ab2[c]]d') == 'abccabccabccd'
-    assert decode_string(s='3[abc]2[d]') == 'abcabcabcdd'
-    assert decode_string(s='abc2[d]') == 'abcdd'
-    assert decode_string(s='3[abc]') == 'abcabcabc'
-    assert decode_string(s='13[ab[c[d]]]') == 'abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd'
-
-
-main()
+assert solve([5, -50, 5, 5, -50, -1, -1, -5, -3, 100]) == 2
+assert solve([10, -10, -1, -1, 10]) == 1
+assert solve([-1, -1, -1, 1, 1, 1, 1]) == 3
+assert solve([5, -2 -3, 1]) == 0
