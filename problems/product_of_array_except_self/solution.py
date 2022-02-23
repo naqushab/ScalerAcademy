e3385d0bc67f731f08870300c2fa0a9d33cb8f51
Naqushab Neyazee <neyazee@adobe.com>
2022-02-23 10:42:54 +0000

    Sync LeetCode submission - Product of Array Except Self (python3)

--- problems/product_of_array_except_self/solution.py
+++ problems/product_of_array_except_self/solution.py
@@ -1,23 +1,23 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        prefix_prod, suffix_prod = [1]*len(nums), [1]*len(nums)
-        for i in range(len(nums)):
+        n = len(nums)
+        prefix_prod, suffix_prod = [1]*n, [1]*n
+        for i in range(n):
+            s = n-i-1
             if i == 0:
                 prefix_prod[i] = nums[i]
+                suffix_prod[s] = nums[s]
             else:
-                prefix_prod[i] = prefix_prod[i-1]*nums[i]
-        for i in range(len(nums)-1, -1, -1):
-            if i == len(nums)-1:
-                suffix_prod[i] = nums[i]
-            else:
-                suffix_prod[i] = nums[i]*suffix_prod[i+1]
-        ans = [1]*len(nums)
-        
-        for i in range(len(nums)):
+                prefix_prod[i] = prefix_prod[i-1] * nums[i]
+                suffix_prod[s] = suffix_prod[s+1] * nums[s]
+        ans = [1]*n
+        for i in range(n):
             if i == 0:
                 ans[i] = suffix_prod[i+1]
-            elif i == len(nums)-1:
+            elif i == n-1:
                 ans[i] = prefix_prod[i-1]
             else:
-                ans[i] = prefix_prod[i-1]*suffix_prod[i+1]
+                ans[i] = prefix_prod[i-1] * suffix_prod[i+1]
         return ans
+                
+        
