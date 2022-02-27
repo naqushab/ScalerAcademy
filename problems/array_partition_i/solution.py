8016f11cc759fa7f99f5a47566851de617d11c13
Naqushab Neyazee <neyazee@adobe.com>
2022-02-27 21:50:29 +0000

    Sync LeetCode submission - Array Partition I (python3)

---
+++ problems/array_partition_i/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+        ans = 0
+        for i in range(0, len(nums), 2):
+            ans += nums[i]
+        return ans
