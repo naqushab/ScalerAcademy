6bdca9d5f77989c0f85dd3986ab97d0de839ce08
Naqushab Neyazee <neyazee@adobe.com>
2022-03-10 01:30:43 +0000

    Sync LeetCode submission - Add Two Numbers (python3)

--- problems/add_two_numbers/solution.py
+++ problems/add_two_numbers/solution.py
@@ -8,15 +8,14 @@ class Solution:
         dummy = ans = ListNode(0)
         carry = 0
         while l1 or l2 or carry:
-            s = carry
             if l1:
-                s += l1.val
+                carry += l1.val
                 l1 = l1.next
             if l2:
-                s += l2.val
+                carry += l2.val
                 l2 = l2.next
-            ans.next = ListNode(s%10)
+            num, carry = carry%10, carry//10
+            ans.next = ListNode(num)
             ans = ans.next
-            carry = s//10
-        ans.next = None
         return dummy.next
+            
