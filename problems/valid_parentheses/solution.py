e9cbcbd3244413c103837ee1b77c2fd7863527b9
Naqushab Neyazee <neyazee@adobe.com>
2022-03-13 00:20:04 +0000

    Sync LeetCode submission - Valid Parentheses (python3)

--- problems/valid_parentheses/solution.py
+++ problems/valid_parentheses/solution.py
@@ -1,20 +1,16 @@
 from collections import deque
 class Solution:
     def isValid(self, s: str) -> bool:
-        m = {
-            '}': '{',
-            ']': '[',
-            ')': '('
-        }
         st = deque()
+        m = { ')': '(', ']': '[', '}': '{' }
         for ch in s:
             if ch in m.values():
                 st.append(ch)
-            elif ch in m:
-                if len(st) == 0:
+            if ch in m.keys():
+                if not st:
                     return False
-                elif st[-1] != m[ch]:
-                    return False
-                elif st[-1] == m[ch]:
+                if st[-1] == m[ch]:
                     st.pop()
+                else:
+                    return False
         return len(st) == 0
