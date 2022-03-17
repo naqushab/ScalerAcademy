df3a5965e17d408c3dcebb78a9ceb2f50c43aec2
Naqushab Neyazee <neyazee@adobe.com>
2022-03-17 12:26:26 +0000

    Sync LeetCode submission - Koko Eating Bananas (python3)

--- problems/koko_eating_bananas/solution.py
+++ problems/koko_eating_bananas/solution.py
@@ -1,13 +1,17 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        def feasible(n):
-            return sum([math.ceil(pile/n) for pile in piles]) <= h
-        
         l, r = 1, max(piles)
-        while l < r:
+        while l <= r:
             m = (l+r)//2
-            if feasible(m):
-                r = m
+            if self.can_eat(piles, h, m):
+                ans = m
+                r = m-1
             else:
                 l = m+1
-        return l
+        return ans
+    
+    def can_eat(self, piles, h, m):
+        time = 0
+        for i in range(len(piles)):
+            time += math.ceil(piles[i]/m)
+        return time <= h
