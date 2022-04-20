27ad483269699a44f7c5a6471f3a55c0123e9f97
Naqushab Neyazee <neyazee@adobe.com>
2022-04-20 20:53:23 +0000

    Sync LeetCode submission - Basic Calculator II (python3)

--- problems/basic_calculator_ii/solution.py
+++ problems/basic_calculator_ii/solution.py
@@ -3,8 +3,7 @@ from collections import deque
 class Solution:
     def calculate(self, s: str) -> int:
         st = deque()
-        last_num = 0
-        last_op = '+'
+        ans, last_num, last_op = 0, 0, '+'
         for i in range(len(s)):
             if s[i].isdigit():
                 last_num = last_num*10 + int(s[i])
@@ -16,17 +15,9 @@ class Solution:
                 elif last_op == '*':
                     st.append(st.pop() * last_num)
                 elif last_op == '/':
-                    temp = st.pop() / last_num
-                    if temp < 0 and temp//1.0 != temp:
-                        temp = math.floor(temp) + 1
-                    else:
-                        temp = math.floor(temp)
-                    st.append(temp)
+                    st.append(int(st.pop() / last_num))
                 last_op = s[i]
                 last_num = 0
-        
-        ans = 0
         while st:
             ans += st.pop()
         return ans
-                
