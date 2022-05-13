7bfc729b53de374426dd3dab1b7f6b3d512407dc
Naqushab Neyazee <neyazee@adobe.com>
2022-05-13 09:19:14 +0000

    Sync LeetCode submission - Same Tree (python3)

--- problems/same_tree/solution.py
+++ problems/same_tree/solution.py
@@ -8,9 +8,6 @@ class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         if not p and not q:
             return True
-        elif p and not q:
+        if not p or not q:
             return False
-        elif not p and q:
-            return False
-        else:
-            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
