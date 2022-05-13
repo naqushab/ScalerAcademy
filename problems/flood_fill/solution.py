10bd23bd94ed006a23cd8fb2f0138c2266835741
Naqushab Neyazee <neyazee@adobe.com>
2022-05-13 10:34:10 +0000

    Sync LeetCode submission - Flood Fill (python3)

--- problems/flood_fill/solution.py
+++ problems/flood_fill/solution.py
@@ -1,22 +1,20 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
-        r = len(image)
-        c = len(image[0])
-        clr = image[sr][sc]
-        if clr == newColor:
+        r, c = len(image), len(image[0])
+        if sr >= r or sr < 0 or sc >= c or sc < 0 or image[sr][sc] == newColor:
             return image
         
-        def dfs(image, i, j):
-            if image[i][j] == clr:
-                image[i][j] = newColor
-                if 0<=i-1<r and 0<=j<c:
-                    dfs(image, i-1, j)
-                if 0<=i+1<r and 0<=j<c:
-                    dfs(image, i+1, j)
-                if 0<=i<r and 0<=j-1<c:
-                    dfs(image, i, j-1)
-                if 0<=i<r and 0<=j+1<c:
-                    dfs(image, i, j+1)
+        def flood_fill(orig_color, new_color, sr, sc):
+            if sr >= r or sr < 0 or sc >= c or sc < 0:
+                return
+            if image[sr][sc] == orig_color:
+                image[sr][sc] = new_color
+            else:
+                return
+            for (x, y) in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                new_sr, new_sc = sr + x, sc + y
+                flood_fill(orig_color, new_color, new_sr, new_sc)
         
-        dfs(image, sr, sc)
+        orig_color = image[sr][sc]
+        flood_fill(orig_color, newColor, sr, sc)
         return image
