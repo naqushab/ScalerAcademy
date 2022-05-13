2ade7b484053cbe2babc9afe240c42f8214ce0bd
Naqushab Neyazee <neyazee@adobe.com>
2022-05-13 10:21:30 +0000

    Sync LeetCode submission - Ransom Note (python3)

--- problems/ransom_note/solution.py
+++ problems/ransom_note/solution.py
@@ -1,11 +1,12 @@
-from collections import Counter
-
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        hm_rn = Counter(ransomNote)
-        hm_mag = Counter(magazine)
-        
-        for k, v in hm_rn.items():
-            if k not in hm_mag or v > hm_mag[k]:
+        magazine_counter = collections.Counter(magazine)
+        for ch in ransomNote:
+            if ch in magazine_counter:
+                magazine_counter[ch] -= 1
+                if magazine_counter[ch] == 0:
+                    del magazine_counter[ch]
+            else:
                 return False
         return True
+                
