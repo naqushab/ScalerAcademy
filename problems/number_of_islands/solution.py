f6057cdaa6ac05339d557a800aa314e2a387ed33
Naqushab Neyazee <neyazee@adobe.com>
2022-05-14 00:50:57 +0000

    Sync LeetCode submission - Number of Islands (python3)

--- problems/number_of_islands/solution.py
+++ problems/number_of_islands/solution.py
@@ -1,22 +1,19 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def dfs(grid, i, j):
+        r, c = len(grid), len(grid[0])
+        
+        def mark_ones(m, n):
+            if 0 <= m < r and 0 <= n < c and grid[m][n] == '1':
+                grid[m][n] = '-1'
+                for x, y in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                    new_m, new_n = m + x, n + y
+                    mark_ones(new_m, new_n)
+            return
+
+        count = 0
+        for i, j in itertools.product(range(r), range(c)):
             if grid[i][j] == '1':
-                grid[i][j] = '0'
-                r = len(grid)
-                c = len(grid[0])
-                if 0<=i-1<=r-1 and 0<=j<=c-1:
-                    dfs(grid, i-1, j)
-                if 0<=i+1<=r-1 and 0<=j<=c-1:
-                    dfs(grid, i+1, j)
-                if 0<=i<=r-1 and 0<=j-1<=c-1:
-                    dfs(grid, i, j-1)
-                if 0<=i<=r-1 and 0<=j+1<=c-1:
-                    dfs(grid, i, j+1)
-        ans = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == '1':
-                    ans += 1
-                    dfs(grid, i, j)
-        return ans
+                count += 1
+                mark_ones(i, j)
+        
+        return count
