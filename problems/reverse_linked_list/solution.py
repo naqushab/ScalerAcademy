39c60254eb9ccdf631e2bfc88aa922b461d68631
Naqushab Neyazee <neyazee@adobe.com>
2022-06-02 09:49:37 +0000

    Sync LeetCode submission - Reverse Linked List (python3)

--- problems/reverse_linked_list/solution.py
+++ problems/reverse_linked_list/solution.py
@@ -5,6 +5,14 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        rev = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return rev
+    
+    def rev(self, head):
         prev, cur = None, head
         while cur:
             cur.next, cur, prev = prev, cur.next, cur
